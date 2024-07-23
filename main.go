02b5cc5e56b37bbf946bfa5f15ce3f4e87aca311
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-07-23 14:39:50 +0100

    feat: variables, strings & numbers

--- main.go
+++ main.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello Chris")
+
+	// Declaring strings
+	// var nameOne string = "Hello"
+	// var nameTwo = "Chris"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameTwo = "Jake"
+	// nameThree = "Miracle"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "Yahsi"
+
+	// fmt.Println(nameFour)
+
+	//Declaring Ints
+
+	var ageOne int = 40
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// intbits
+
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint16 = 256
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 8893.2938
+	scoreThree := 6.3
 }

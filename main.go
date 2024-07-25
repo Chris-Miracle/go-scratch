f905fe9cdc611dc10e50518361ed7ec510a11edc
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-07-25 23:00:04 +0100

    maps

--- main.go
+++ main.go
@@ -4,16 +4,39 @@ import (
 	"fmt"
 )
 
-var score = 99.9
-
 
 func main() {
 	
-	sayHello("Chris")
-
-	for _, value := range points {
-		fmt.Println(value)
+	menu := map[string]float64{
+		"soup" : 4.5,
+		"pie" : 7.99,
+		"salad" : 6.99,
+		"pudding" : 3.55,
 	}
 
-	showScore()
+	fmt.Println(menu)
+
+	fmt.Println(menu["pie"])
+
+	for k, v := range menu {
+		fmt.Println(k, "-", v)
+	}
+
+	// ints as key type
+	phonebook := map[int]string{
+		1234567890 : "mario",
+		9876543210 : "luigi",
+		1111111111 : "yoshi",
+	}
+
+	fmt.Println(phonebook)
+	fmt.Println(phonebook[1234567890])
+
+	phonebook[1234567890] = "mario2"
+
+	fmt.Println(phonebook)
+
+	phonebook[1111111111] = "jake"
+
+	fmt.Println(phonebook)
 }

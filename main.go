6ee3b993d4b76d915b9d2632e59a5d5a49d4b2aa
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-07-25 22:00:43 +0100

    package scope

--- main.go
+++ main.go
@@ -2,32 +2,18 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
 
-func getInitials(n string) (string, string) {
-	s := strings.ToUpper(n)
-	names := strings.Split(s, " ")
+var score = 99.9
 
-	var initials []string
-	for _, v := range names {
-		initials = append(initials, v[:1])
-	}
-
-	if len(initials) > 1 {
-		return initials[0], initials[1]
-	}
-
-	return initials[0], "_"
-}
 
 func main() {
-	fn1, sn1 := getInitials("tifa lockhart")
-	fmt.Println(fn1, sn1)
+	
+	sayHello("Chris")
 
-	fn2, sn2 := getInitials("cloud strife")
-	fmt.Println(fn2, sn2)
+	for _, value := range points {
+		fmt.Println(value)
+	}
 
-	fn3, sn3 := getInitials("barret")
-	fmt.Println(fn3, sn3)
+	showScore()
 }

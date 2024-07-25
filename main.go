55727b70b00a78a28ce220da977a4be4352ef14a
Chris-Miracle <chrisstarkmira15@gmail.com>
2024-07-25 23:31:02 +0100

    pointers

--- main.go
+++ main.go
@@ -4,32 +4,20 @@ import (
 	"fmt"
 )
 
-func updateName(name string) string {
-	name = "Nairobi"
-	return name
+func updateName(name *string) {
+	*name = "Nairobi"
 }
-
-func updateMenu(menu map[string]float64) {
-	menu["Burger"] = 1000.09
-}
-
 
 func main() {
 	
 	// group A types -> string, int, float, bool, arrays, structs
 	name := "Ikeja"
 
-	name = updateName(name)
+
+	m := &name
 
 	fmt.Println(name)
+	updateName(m)
+	fmt.Println(name)
 
-	// group B types -> slices, maps, functions
-	menu := map[string]float64{
-		"Burger": 10.0,
-		"Pizza": 20.0,
-		"Fries": 1.50,
-	}
-
-	updateMenu(menu)
-	fmt.Println(menu)
 }
